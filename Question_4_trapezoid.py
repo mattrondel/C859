5874f7cc656e140a1f2ad3fcdb3488ce98af881a
Matt Rondel <109989470+mattrondel@users.noreply.github.com>
2023-12-20 13:42:27 -0800

    Update and rename Question_4.py to Question_4_trapezoid.py

---
+++ Question_4_trapezoid.py
@@ -0,0 +1,11 @@
+
+# Accept three integer inputs for base (b1, b2) and height (h)
+b1 = int(input())
+b2 = int(input())
+h = int(input())
+
+# Calculate the trapezoid area
+area = ((b1 + b2) / 2) * h
+
+# Output the result in the required format
+print(f'Trapezoid area: {area} square meters')
