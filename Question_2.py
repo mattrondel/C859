dee43ce5fae4341ebaa61512ebc8c0c4acfe3e3b
Matt Rondel <109989470+mattrondel@users.noreply.github.com>
2023-12-19 16:05:37 -0800

    Update Question_2.py

--- Question_2.py
+++ Question_2.py
@@ -1,1 +1,20 @@
+def convert_ounces(ounces):
+    # Calculate tons, pounds, and remaining ounces
+    tons = ounces // (16 * 2000)
+    remaining_ounces = ounces % (16 * 2000)
+    pounds = remaining_ounces // 16
+    ounces = remaining_ounces % 16
 
+    # Output the result in the required format
+    print(f"Tons: {tons}")
+    print(f"Pounds: {pounds}")
+    print(f"Ounces: {ounces}")
+
+# Get input from the user
+input_ounces = int(input("Enter the number of ounces: "))
+
+# Call the function with the input
+convert_ounces(input_ounces)
+
+
+You can run this script, enter the number of ounces when prompted, and it will display the converted result in the specified format.
