8d01e2bb70ae949d6bead9d896401d37be33174f
Matt Rondel <109989470+mattrondel@users.noreply.github.com>
2024-01-05 15:59:24 -0800

    Update Question_6_Format_String_Phone_number.py

--- Question_6_Format_String_Phone_number.py
+++ Question_6_Format_String_Phone_number.py
@@ -1,8 +1,12 @@
 # https://learn.zybooks.com/zybook/WGUC859v4/chapter/34/section/6
 
-# "without hard-coded information, allowing the function to handle different input values"
-
-
-
-
-#This code defines a function format_student_id that takes an integer input, converts it to a string, and then formats it according to the specified pattern. The sample input is provided, and the output is printed, producing the desired result.
+# Task:
+# Create a solution that accepts an integer input representing a 9-digit unformatted student identification number. Output the identification number as a string with no spaces.
+# The solution output should be in the format
+# 111-22-3333
+#  
+# Sample Input/Output:
+# If the input is
+# 154175430
+# then the expected output is
+# 154-17-5430
