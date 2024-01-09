18f3c857db0df32bce4d027d402b0c0e3dea4b92
Matt Rondel <109989470+mattrondel@users.noreply.github.com>
2024-01-09 15:49:09 -0800

    Update Question_5_Five_Input.py

--- Question_5_Five_Input.py
+++ Question_5_Five_Input.py
@@ -1,34 +1,34 @@
-# https://learn.zybooks.com/zybook/WGUC859v4/chapter/34/section/5
+#  https://learn.zybooks.com/zybook/WGUC859v4/chapter/34/section/5
+  
+#  
+# Task:
+# Create a solution that accepts five integer inputs. 
+# Output the sum of the five inputs three times, converting the inputs to the requested data type prior to finding the sum.
+  
+# First output: sum of five inputs maintained as integer values
+# Second output: sum of five inputs converted to float values
+# Third output: sum of five inputs converted to string values (concatenate)
+# The solution output should be in the format
+# Integer: integer_sum_value Float: float_sum_value String: string_sum_value
+#  
+# Sample Input/Output:
+# If the input is
+# 1
+# 3
+# 6
+# 2
+# 7
+# then the expected output is
+# Integer: 19
+# Float: 19.0
+# String: 13627
 
-# 
-#Task:
-#Create a solution that accepts five integer inputs. 
-#Output the sum of the five inputs three times, converting the inputs to the requested data type prior to finding the sum.
- 
-#First output: sum of five inputs maintained as integer values
-#Second output: sum of five inputs converted to float values
-#Third output: sum of five inputs converted to string values (concatenate)
-#The solution output should be in the format
-#Integer: integer_sum_value Float: float_sum_value String: string_sum_value
-# 
-#Sample Input/Output:
-#If the input is
-#1
-#3
-#6
-#2
-#7
-#then the expected output is
-#Integer: 19
-#Float: 19.0
-#String: 13627
 
-
-#solution accepts five integer inputs
-#solution outputs three sums of input values; convert before calculating sum
-#first output: sum of five inputs maintained as integer values
-#second output: sum of five inputs converted to float values
-#third output: sum of five inputs converted to string values (concatenate)
+# solution accepts five integer inputs
+# solution outputs three sums of input values; convert before calculating sum
+# first output: sum of five inputs maintained as integer values
+# second output: sum of five inputs converted to float values
+# third output: sum of five inputs converted to string values (concatenate)
 # Accepting five integer inputs
 num1 = int(input())
 num2 = int(input())
