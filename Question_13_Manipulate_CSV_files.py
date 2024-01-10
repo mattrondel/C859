a52b6c9694668b6720662467477d0b783d4c0b94
Matt Rondel <109989470+mattrondel@users.noreply.github.com>
2024-01-10 09:01:35 -0800

    Update Question_13_Manipulate_CSV_files.py

--- Question_13_Manipulate_CSV_files.py
+++ Question_13_Manipulate_CSV_files.py
@@ -1,1 +1,18 @@
+# https://learn.zybooks.com/zybook/WGUC859v4/chapter/34/section/13
 
+# Create a solution that accepts an input identifying the name of a CSV file, for example, "input1.csv". Each file contains two rows of comma-separated values. Import the built-in module csv and use its open() function and reader() method to create a dictionary of key:value pairs for each row of comma-separated values in the specified file. Output the file contents as two dictionaries.
+# The solution output should be in the format
+# {key:value,key:value,key:value}
+# {key:value,key:value,key:value}
+#  
+# Sample Input/Output:
+# If the file content is
+# input1.csv
+# then the expected output is
+# {a:100,b:200,c:300}
+# {bananas:1.85,steak:19.99,cookies:4.52}
+# Alternatively, if the file content is
+# input2.csv
+# then the expected output is
+# {d:400,e:500,f:600}
+# {celery:2.81,milk:4.34,bread:5.63}
