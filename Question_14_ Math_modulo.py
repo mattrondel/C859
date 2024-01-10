2670fa635771569bdf82575230085957c0124c95
Matt Rondel <109989470+mattrondel@users.noreply.github.com>
2024-01-10 09:10:22 -0800

    Create Question_14_ Math_modulo.py

---
+++ Question_14_ Math_modulo.py
@@ -0,0 +1,18 @@
+# https://learn.zybooks.com/zybook/WGUC859v4/chapter/34/section/14
+
+# Create a solution that accepts an integer input. Import the built-in module math and use its factorial() method to calculate the factorial of the integer input. Output the value of the factorial, as well as a Boolean value identifying whether the factorial output is greater than 100.
+# The solution output should be in the format
+# factorial_value
+# Boolean_value
+#  
+# Sample Input/Output:
+# If the input is
+# 10
+# then the expected output is
+# 3628800
+# True
+# Alternatively, if the input is
+# 3
+# then the expected output is
+# 6
+# False
