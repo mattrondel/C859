b8ff28d9e8e8a6bbec60ce6e7b0d8f7ed945836c
Matt Rondel <109989470+mattrondel@users.noreply.github.com>
2024-01-10 08:53:43 -0800

    Update Question_2_any_number_of_ounces.py

--- Question_2_any_number_of_ounces.py
+++ Question_2_any_number_of_ounces.py
@@ -1,5 +1,20 @@
 # https://learn.zybooks.com/zybook/WGUC859v4/chapter/34/section/2
 
+# Create a solution that accepts an integer input representing any number of ounces. Output the converted total number of tons, pounds, and remaining ounces based on the input ounces value. There are 16 ounces in a pound and 2,000 pounds in a ton.
+# The solution output should be in the format
+# Tons: value_1
+# Pounds: value_2
+# Ounces: value_3
+#  
+# Sample Input/Output:
+# If the input is
+# 32035
+# then the expected output is
+# Tons: 1
+# Pounds: 2
+# Ounces: 3
+
+
 def convert_ounces(ounces):
     # Calculate tons, pounds, and remaining ounces
     tons = ounces // (16 * 2000)
