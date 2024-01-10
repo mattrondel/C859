45fd7fbe1613f1ef51470cb20378eb89c8668f96
Matt Rondel <109989470+mattrondel@users.noreply.github.com>
2024-01-10 08:59:30 -0800

    Update Question_12_Manipulate_text_files.py

--- Question_12_Manipulate_text_files.py
+++ Question_12_Manipulate_text_files.py
@@ -1,7 +1,8 @@
 
 # https://learn.zybooks.com/zybook/WGUC859v4/chapter/34/section/12
 
-# Create a solution that opens a file and combines the words on each line into a sentence. Write the sentence string to the end of the text file "WordTextFile1.txt" on a new line. Output the new contents of "WordTextFile1.txt". Use the open() function and write(), read() methods to interact with the text file.
+# Create a solution that opens a file and combines the words on each line into a sentence. Write the sentence string to the end of the text file "WordTextFile1.txt" on a new line. 
+# Output the new contents of "WordTextFile1.txt". Use the open() function and write(), read() methods to interact with the text file.
 
 # The solution output should be in the format:
 
