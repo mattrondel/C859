45a2c12daf6519303de1cf1bb2d02e0fa427d198
Matt Rondel <109989470+mattrondel@users.noreply.github.com>
2024-01-10 14:03:45 -0800

    Update Question_9_Branching_conditional_logic.py

--- Question_9_Branching_conditional_logic.py
+++ Question_9_Branching_conditional_logic.py
@@ -1,5 +1,7 @@
 # https://learn.zybooks.com/zybook/WGUC859v4/chapter/34/section/9
 
+# Help me solve and understand this python problem and also solve it so it can accept any input, remove any error checking, do this without a try and except and not with a funtion and have it condensed and simplifed
+
 # Create a solution that accepts an integer input representing water temperature in degrees Fahrenheit. Output a description of the water state based on the following scale:
 #  
 # If the temperature is below 33° F, the water is “Frozen”.
