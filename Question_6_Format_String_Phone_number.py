20797b936ff21a436b11b6a2210de42b4b113500
Matt Rondel <109989470+mattrondel@users.noreply.github.com>
2024-01-10 13:46:56 -0800

    Update Question_6_Format_String_Phone_number.py

--- Question_6_Format_String_Phone_number.py
+++ Question_6_Format_String_Phone_number.py
@@ -1,5 +1,7 @@
 # https://learn.zybooks.com/zybook/WGUC859v4/chapter/34/section/6
 
+# Help me solve and understand this python problem and also solve it so it can accept any input, remove any error checking, do this without a try and except and not with a funtion and ha
+
 # Task:
 # Create a solution that accepts an integer input representing a 9-digit unformatted student identification number. Output the identification number as a string with no spaces.
 # The solution output should be in the format
