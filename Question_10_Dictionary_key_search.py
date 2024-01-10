f9467624cf317a4a6c52896f38cb721685e2bc2f
Matt Rondel <109989470+mattrondel@users.noreply.github.com>
2024-01-10 08:53:03 -0800

    Update Question_10_Dictionary_key_search.py

--- Question_10_Dictionary_key_search.py
+++ Question_10_Dictionary_key_search.py
@@ -1,1 +1,16 @@
+# https://learn.zybooks.com/zybook/WGUC859v4/chapter/34/section/10
 
+# Create a solution that accepts an integer input identifying how many shares of stock are to be purchased from the Old Town Stock Exchange, followed by an equivalent number of string inputs representing the stock selections. The following dictionary stock lists available stock selections as the key with the cost per selection as the value.
+# stocks = {'TSLA': 912.86 , 'BBBY': 24.84, 'AAPL': 174.26, 'SOFI': 6.92, 'KIRK': 8.72, 'AURA': 22.12, 'AMZN': 141.28, 'EMBK': 12.29, 'LVLU': 2.33}
+# Output the total cost of the purchased shares of stock to two decimal places.
+# The solution output should be in the format
+# Total price: $cost_of_stocks
+#  
+# Sample Input/Output:
+# If the input is
+# 3
+# SOFI
+# AMZN
+# LVLU
+# then the expected output is
+# Total price: $150.53
