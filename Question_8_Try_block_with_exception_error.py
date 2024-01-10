358b73d0ec8d4aad362b03c963295ecef2178ad6
Matt Rondel <109989470+mattrondel@users.noreply.github.com>
2024-01-10 13:55:33 -0800

    Update Question_8_Try_block_with_exception_error.py

--- Question_8_Try_block_with_exception_error.py
+++ Question_8_Try_block_with_exception_error.py
@@ -1,5 +1,7 @@
 # https://learn.zybooks.com/zybook/WGUC859v4/chapter/34/section/8
 
+# Help me solve and understand this python problem and also solve it so it can accept any input, remove any error checking, do this without a try and except and not with a funtion and have it condensed and simplifed
+
 # Create a solution that accepts one integer input representing the index value for any of the string elements in the following list:
 # frameworks = ["Django", "Flask", "CherryPy", "Bottle", "Web2Py", "TurboGears"]
 # Output the string element of the index value entered. The solution should be placed in a try block and implement an exception of "Error" if an incompatible integer input is provided.
