61301c691e4dad2d80fef8d7c3a49f190347d57a
Matt Rondel <109989470+mattrondel@users.noreply.github.com>
2024-01-10 09:12:29 -0800

    Create Question_15_Import_custom_module.py

---
+++ Question_15_Import_custom_module.py
@@ -0,0 +1,11 @@
+# https://learn.zybooks.com/zybook/WGUC859v4/chapter/34/section/15
+
+# Create a solution that accepts an integer input representing the age of a pig. Import the existing module pigAge and use its pre-built pigAge_converter() function to calculate the human equivalent age of a pig. A year in a pig's life is equivalent to five years in a human's life. Output the human-equivalent age of the pig.
+# The solution output should be in the format
+# input_pig_age is converted_pig_age in human years
+#  
+# Sample Input/Output:
+# If the input is
+# 8
+# then the expected output is
+# 8 is 40 in human years
