7ebe300537c68106659996fd9729dd135c734c85
Matt Rondel <109989470+mattrondel@users.noreply.github.com>
2024-01-11 14:24:50 -0800

    Update Question_10_Dictionary_key_search.py

--- Question_10_Dictionary_key_search.py
+++ Question_10_Dictionary_key_search.py
@@ -1,5 +1,7 @@
 # https://learn.zybooks.com/zybook/WGUC859v4/chapter/34/section/10
 
+# Help me solve and understand this python problem and also solve it so it can accept any input, remove any error checking, do this without a try and except and not with a function and have it condensed and simplifed and break this down step by step so I can understand this
+
 # Create a solution that accepts an integer input identifying how many shares of stock are to be purchased from the Old Town Stock Exchange, followed by an equivalent number of string inputs representing the stock selections. The following dictionary stock lists available stock selections as the key with the cost per selection as the value.
 # stocks = {'TSLA': 912.86 , 'BBBY': 24.84, 'AAPL': 174.26, 'SOFI': 6.92, 'KIRK': 8.72, 'AURA': 22.12, 'AMZN': 141.28, 'EMBK': 12.29, 'LVLU': 2.33}
 # Output the total cost of the purchased shares of stock to two decimal places.
