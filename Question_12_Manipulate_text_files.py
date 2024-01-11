ee7c957e3c0494ee44a3af9d196594cc73b5c12a
Matt Rondel <109989470+mattrondel@users.noreply.github.com>
2024-01-11 14:03:30 -0800

    Update Question_12_Manipulate_text_files.py

--- Question_12_Manipulate_text_files.py
+++ Question_12_Manipulate_text_files.py
@@ -1,6 +1,8 @@
 
 # https://learn.zybooks.com/zybook/WGUC859v4/chapter/34/section/12
 
+# Help me solve and understand this python problem and also solve it so it can accept any input, remove any error checking, do this without a try and except and not with a funtion and have it condensed and simplifed
+
 # Create a solution that opens a file and combines the words on each line into a sentence. Write the sentence string to the end of the text file "WordTextFile1.txt" on a new line. 
 # Output the new contents of "WordTextFile1.txt". Use the open() function and write(), read() methods to interact with the text file.
 
