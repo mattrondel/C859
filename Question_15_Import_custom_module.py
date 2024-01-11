21910a4b9840284183a265ff422f781015fcd2d4
Matt Rondel <109989470+mattrondel@users.noreply.github.com>
2024-01-11 14:21:06 -0800

    Update Question_15_Import_custom_module.py

--- Question_15_Import_custom_module.py
+++ Question_15_Import_custom_module.py
@@ -1,7 +1,6 @@
 # https://learn.zybooks.com/zybook/WGUC859v4/chapter/34/section/15
 
-# Help me solve and understand this python problem and also solve it so it can accept any input, remove any error checking, do this with a try and except and not with a function and have it condensed and simplifed
-# can you break this down step by step so I can understand this
+# Help me solve and understand this python problem and also solve it so it can accept any input, remove any error checking, do this without a try and except and not with a function and have it condensed and simplifed and break this down step by step so I can understand this
 
 # Create a solution that accepts an integer input representing the age of a pig. Import the existing module pigAge and use its pre-built pigAge_converter() function to calculate the human equivalent age of a pig. A year in a pig's life is equivalent to five years in a human's life. Output the human-equivalent age of the pig.
 # The solution output should be in the format
