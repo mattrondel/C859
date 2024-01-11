5bd990d8b0aefe463df3cdc187da2b88dbe7d6cc
Matt Rondel <109989470+mattrondel@users.noreply.github.com>
2024-01-11 14:05:02 -0800

    Update Question_14_ Math_modulo.py

--- Question_14_ Math_modulo.py
+++ Question_14_ Math_modulo.py
@@ -1,6 +1,8 @@
 # https://learn.zybooks.com/zybook/WGUC859v4/chapter/34/section/14
 
-# Help me solve and understand this python problem and also solve it so it can accept any input, remove any error checking, do this without a try and except and not with a funtion and have it condensed and simplifed
+
+# Help me solve and understand this python problem and also solve it so it can accept any input, remove any error checking, do this with a try and except and not with a function and have it condensed and simplifed
+# can you break this down step by step so I can understand this
 
 # Create a solution that accepts an integer input. Import the built-in module math and use its factorial() method to calculate the factorial of the integer input. Output the value of the factorial, as well as a Boolean value identifying whether the factorial output is greater than 100.
 # The solution output should be in the format
