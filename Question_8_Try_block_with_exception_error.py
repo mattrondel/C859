4aa3c3ebe4f476cc330ff8ba2f79da6c30c209bf
Matt Rondel <109989470+mattrondel@users.noreply.github.com>
2024-01-11 14:21:37 -0800

    Update Question_8_Try_block_with_exception_error.py

--- Question_8_Try_block_with_exception_error.py
+++ Question_8_Try_block_with_exception_error.py
@@ -1,7 +1,6 @@
 # https://learn.zybooks.com/zybook/WGUC859v4/chapter/34/section/8
 
-# Help me solve and understand this python problem and also solve it so it can accept any input, remove any error checking, do this with a try and except and not with a function and have it condensed and simplifed
-# can you break this down step by step so I can understand this
+# Help me solve and understand this python problem and also solve it so it can accept any input, remove any error checking, do this without a try and except and not with a function and have it condensed and simplifed and break this down step by step so I can understand this
 
 # Create a solution that accepts one integer input representing the index value for any of the string elements in the following list:
 # frameworks = ["Django", "Flask", "CherryPy", "Bottle", "Web2Py", "TurboGears"]
