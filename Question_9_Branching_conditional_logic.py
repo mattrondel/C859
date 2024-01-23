d79f267bb32944c5e90b7d86fc6b1675aebba296
Matt Rondel <109989470+mattrondel@users.noreply.github.com>
2024-01-23 11:09:12 -0800

    Update Question_9_Branching_conditional_logic.py

--- Question_9_Branching_conditional_logic.py
+++ Question_9_Branching_conditional_logic.py
@@ -1,5 +1,10 @@
 # https://learn.zybooks.com/zybook/WGUC859v4/chapter/34/section/9
 
+# Need to distinguash between if vs elif
+# Don't forget the the colon after the IF elif statements!
+# if temp < 33:
+ #   state = "Frozen"
+
 # Help me solve and understand this python problem and also solve it so it can accept any input, remove any error checking, do this without a try and except and not with a funtion and have it condensed and simplifed
 
 # Create a solution that accepts an integer input representing water temperature in degrees Fahrenheit. Output a description of the water state based on the following scale:
@@ -47,3 +52,21 @@ print(state)
 print(safety_comment, end='')
 
 # the bigest gotcha is the \n new lines
+
+# Statements broken down my attmpt to figure it out by statements:
+
+# If the temperature is below 33° F, the water is “Frozen”.
+if temp < 33:
+    state = "Frozen"
+
+# If the water is between 33° F and 80° F (including 33), the water is “Cold”.
+if temp <= 80 
+    state = "Cold"
+
+# If the water is between 80° F and 115° F (including 80), the water is "Warm".
+if temp <= 115
+    state = "Warm"
+
+# If the water is between 115° F and 211° (including 115) F, the water is “Hot".
+if temp <= 211
+    state = "Hot"
