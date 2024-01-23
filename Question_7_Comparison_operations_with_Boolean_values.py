7386f6054791d558bc59abb0f88e7e4963072a49
Matt Rondel <109989470+mattrondel@users.noreply.github.com>
2024-01-23 10:49:54 -0800

    Update Question_7_Comparison_operations_with_Boolean_values.py

--- Question_7_Comparison_operations_with_Boolean_values.py
+++ Question_7_Comparison_operations_with_Boolean_values.py
@@ -1,5 +1,5 @@
 # https://learn.zybooks.com/zybook/WGUC859v4/chapter/34/section/7
-#  Explain line 21 of result = input_value > max(predef_list)
+#  Explain line 21 of result = input_value > max(predef_list) and how to interprite the question to a python solution
 #
 # Help me solve and understand this python problem and also solve it so it can accept any input, remove any error checking, do this without a try and except and not with a function and have it condensed and simplifed and break this down step by step so I can understand this
 #
