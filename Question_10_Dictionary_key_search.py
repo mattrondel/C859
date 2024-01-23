bf233daf63055136a7aec10b9b593ad963659704
Matt Rondel <109989470+mattrondel@users.noreply.github.com>
2024-01-23 14:16:03 -0800

    Update Question_10_Dictionary_key_search.py

--- Question_10_Dictionary_key_search.py
+++ Question_10_Dictionary_key_search.py
@@ -5,10 +5,14 @@
 # Help me solve and understand this python problem and also solve it so it can accept any input, remove any error checking, 
 # do this without a try and except and not with a function and have it condensed and simplifed and break this down step by step so I can understand this
 
-# Create a solution that accepts an integer input identifying how many shares of stock are to be purchased from the Old Town Stock Exchange, followed by an equivalent number of string inputs representing the stock selections. The following dictionary stock lists available stock selections as the key with the cost per selection as the value.
+# Create a solution that accepts an integer input identifying how many shares of stock are to be purchased from the Old Town Stock Exchange, followed by an equivalent 
+# number of string inputs representing the stock selections. The following dictionary stock lists available stock selections as the key with the cost per selection as the value.
+# 
 # stocks = {'TSLA': 912.86 , 'BBBY': 24.84, 'AAPL': 174.26, 'SOFI': 6.92, 'KIRK': 8.72, 'AURA': 22.12, 'AMZN': 141.28, 'EMBK': 12.29, 'LVLU': 2.33}
+#
 # Output the total cost of the purchased shares of stock to two decimal places.
 # The solution output should be in the format
+#
 # Total price: $cost_of_stocks
 #  
 # Sample Input/Output:
@@ -24,3 +28,4 @@ total_price = sum(stocks[input().strip()] for _ in range(int(input())))
 print(f'Total price: ${total_price:.2f}')
 
 # two decimal places keyword for :.2f
+# THINK OF THE .2 AS IN .2 DECINAL PLACES AND THE f IS FOR FORMAT
