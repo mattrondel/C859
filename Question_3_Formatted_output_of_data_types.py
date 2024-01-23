b03bec1efbe1e397757ed21ff40db9d4a4420394
Matt Rondel <109989470+mattrondel@users.noreply.github.com>
2024-01-23 08:40:04 -0800

    Update Question_3_Formatted_output_of_data_types.py

--- Question_3_Formatted_output_of_data_types.py
+++ Question_3_Formatted_output_of_data_types.py
@@ -22,6 +22,7 @@ index = int(input())
 various_data_types = [516, 112.49, True, "meow", ("Western", "Governors", "University"), {"apple": 1, "pear": 5}]
 print(f"Element {index}: {type(various_data_types[index]).__name__}")
 
+#".name attribute" is called this but means dunder .name or .__name__
 
 # The .__name__ attribute is used to obtain the name of a class or type in Python. When applied to the result of the type() function, it returns the name of the data type as a string.
 
