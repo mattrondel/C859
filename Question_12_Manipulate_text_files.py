4a5376f89444d2928e6ea1b109463e48ba001985
Matt Rondel <109989470+mattrondel@users.noreply.github.com>
2024-01-23 15:39:24 -0800

    Update Question_12_Manipulate_text_files.py

--- Question_12_Manipulate_text_files.py
+++ Question_12_Manipulate_text_files.py
@@ -2,7 +2,7 @@
 # https://learn.zybooks.com/zybook/WGUC859v4/chapter/34/section/12
 
 ########################################################################################################################
-# This is the file read and can be complicated I need to practice with this, need a break down step-by-step on this
+# This is the file read and can be complicated so I need to practice with this, need a break down step-by-step on this
 ########################################################################################################################
 
 # Help me solve and understand this python problem and also solve it so it can accept any input, remove any error checking, do this without a try and except and not with a function and have it condensed and simplifed and break this down step by step so I can understand this
