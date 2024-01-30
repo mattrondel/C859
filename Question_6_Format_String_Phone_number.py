4ebaafbddf18bdae6279ac567bec421cd46d2402
Matt Rondel <109989470+mattrondel@users.noreply.github.com>
2024-01-30 15:04:11 -0800

    Update Question_6_Format_String_Phone_number.py

--- Question_6_Format_String_Phone_number.py
+++ Question_6_Format_String_Phone_number.py
@@ -29,7 +29,8 @@ print(formatted_output)
 # my_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 #            0, 1, 2, 3, 4, 5, 6, 7, 8, 9
 #          -10,-9,-8,-7,-6,-5,-4,-3,-2,-1
-# print my_list[:]
+# print my_list[1:3]
+# this would output 12
 
 # If you are trying to take a phone number and output it like from 16617226300 you would do something like this:
 #phone = str(16617226300)
