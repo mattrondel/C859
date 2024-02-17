8f608669d8e960e0374be7ea25f615c800b32ebc
Matt Rondel <109989470+mattrondel@users.noreply.github.com>
2024-02-16 16:14:34 -0800

    Update Question_02_any_number_of_ounces.py

--- Question_02_any_number_of_ounces.py
+++ Question_02_any_number_of_ounces.py
@@ -2,6 +2,9 @@
 
 # got wrong on PA
 
+# Derek 
+# -A similar question appeared in my OA, but instead of converting weight (oz, lbs, tons) you had to convert time (minutes, hours, days). The solution and output were almost exactly the same.
+
 # need to know why use a funtion and the benefits of this or find out what this would look like without being a function
 # https://www.youtube.com/watch?v=6iF8Xb7Z3wQ
 
