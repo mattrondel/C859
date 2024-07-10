4f6e41c13d24f6436ca2d8e7fccd5d670ab830ba
Matt Rondel <109989470+mattrondel@users.noreply.github.com>
2024-07-10 14:49:44 -0700

    Update Question_09_Branching_conditional_logic.py

--- Question_09_Branching_conditional_logic.py
+++ Question_09_Branching_conditional_logic.py
@@ -1,5 +1,8 @@
 # https://learn.zybooks.com/zybook/WGUC859v4/chapter/34/section/9
 
+looks like what the exam had
+https://www.bitdegree.org/learn/best-code-editor/python-if-else-example-5
+
 Socratica: If/Else in Python
 https://www.youtube.com/watch?v=f4KOjWS_KZs
 
