ae8579ecddd2123e415cb50c6ccef56adac16645
Matt Rondel <mattrondel@gmail.com>
2024-10-27 21:50:22 -0700

    Update Question_14_ Math_modulo.py

--- Question_14_ Math_modulo.py
+++ Question_14_ Math_modulo.py
@@ -32,13 +32,13 @@ print(Boolean_value)
 
 #####################################################################
 # if Derek is correct then it might look like this:
-probably should get coles help on this
 
 # Need to test the below in the blank zybook lab:
 https://learn.zybooks.com/zybook/WGUC859v4/chapter/2/section/16
 "python use the sqrt() method on an integer input and determine if it is a perfect square"
 
-from claude:
+from claude.ai :
+
 import math
 
 number = int(input())
