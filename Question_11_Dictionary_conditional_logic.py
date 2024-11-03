20882aeff56e4774af58627d9a19db847e619d50
Matt Rondel <mattrondel@gmail.com>
2024-11-02 19:24:47 -0700

    Update Question_11_Dictionary_conditional_logic.py

--- Question_11_Dictionary_conditional_logic.py
+++ Question_11_Dictionary_conditional_logic.py
@@ -7,9 +7,9 @@ https://learn.zybooks.com/zybook/WGUC859v4/chapter/2/section/16
 # consider:
 # https://aiplanet.com/learn/5-week_Data_Science_Bootcamp/week-0/150/conditional-statement-and-dictionaries
 
-# The thing at the bottom is called conditional expression also known as ternary operators
-# zybooks lesson on it
-# https://learn.zybooks.com/zybook/WGUC859v4/chapter/5/section/12
+The thing at the bottom is called conditional expression also known as ternary operators
+zybooks lesson on it
+https://learn.zybooks.com/zybook/WGUC859v4/chapter/5/section/12
 
 # zybooks on Dictonaries
 # https://learn.zybooks.com/zybook/WGUC859v4/chapter/11/section/12
