2e710ccf85878e501f664f83e536fad50ee08c1f
Matt Rondel <mattrondel@gmail.com>
2025-01-28 13:16:29 -0800

    Update Exercises_in_10.10.py

--- Exercises_in_10.10.py
+++ Exercises_in_10.10.py
@@ -1,7 +1,7 @@
 # test here:
 # https://pythontutor.com/render.html#mode=display
 
-# Task 1
+# Task 1 - good
 
 # Complete the function to print the first X number of characters in the given string
 
