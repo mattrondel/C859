75778990610b06d80b3291dbde0768e894e795a8
Matt Rondel <mattrondel@gmail.com>
2025-01-28 16:26:19 -0800

    Create Zybooks_stumpers.py

---
+++ Zybooks_stumpers.py
@@ -0,0 +1,73 @@
+# Zybooks stumpers
+# 22.10 LAB*: Program: Cooking measurement converter
+   
+###########
+
+# FIXME (1): Finish reading other items into variables, then output the three ingredients
+
+lemon_juice_cups = float(input('Enter amount of lemon juice (in cups):\n'))
+
+water = float(input('Enter amount of water (in cups):\n'))
+
+agave = float(input('Enter amount of agave nectar (in cups):\n'))
+
+servings = float(input('How many servings does this make?\n\n'))
+
+# Sample data:
+# 2 lemon
+# 16 water
+# 2.5 agave
+# 6 servings make
+
+# should be: Lemonade ingredients - yields 6.00 servings
+print(f"Lemonade ingredients - yields {servings:.2f} servings")
+
+# should be: 2.00 cup(s) lemon juice
+print(f"{lemon_juice_cups:.2f} cup(s) lemon juice")
+
+# should be: 16.00 cup(s) water
+print(f"{water:.2f} cup(s) water")
+
+# should be: 2.50 cup(s) agave nectar
+print(f"{agave:.2f} cup(s) agave nectar")
+
+
+
+# FIXME (2): Prompt user for desired number of servings. Convert and output the ingredients
+
+
+# test data 48
+howmanservings = float(input("How many servings would you like to make?\n"))
+
+#calc lemon juice from input, HOW WOULD YOU GO ABOUT FIGURING OUT THE EQUATION 
+# IF 48 servings is 16 cups how much is 1 serving, well divide...
+lemon2 = howmanservings / 3
+
+# Calc water... dont freaking know
+# 48 input 
+# 128 output
+# 2.66??
+
+# Calc agave... dont freaking know
+# 48 / 20
+# agave2 = 2.4 
+
+#should be Lemonade ingredients - yields 48.00 servings
+print(f"Lemonade ingredients - yields {howmanservings:.2f} servings")
+
+# should be 16.00 cup(s) lemon juice
+print(f"{lemon2:.2f} cup(s) lemon juice")
+
+# should be 128.00 cup(s) water
+#8.00 cup(s) water
+
+# should be 20.00 cup(s) agave nectar
+print(f"{agave2} cup(s) agave nectar
+
+
+
+
+
+
+# FIXME (3): Convert and output the ingredients from (2) to gallons
+   
